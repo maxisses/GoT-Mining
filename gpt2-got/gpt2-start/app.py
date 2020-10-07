48db3b6b11ed159f41659719b82d13ba399e7543
maxisses <max_dargatz@hotmail.com>
2020-10-07 13:57:21 +0200

    fixed port

--- gpt2-got/gpt2-start/app.py
+++ gpt2-got/gpt2-start/app.py
@@ -72,5 +72,5 @@ async def homepage(request):
                          headers=response_header)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=int(8081))
-    # uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    #uvicorn.run(app, host='0.0.0.0', port=int(8081))
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
